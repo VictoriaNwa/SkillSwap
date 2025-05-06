3e01cac96a10b880842ed70fc4c1014d463f6f2f
Victoria Nwanze <vickyonwa@gmail.com>
2025-05-06 07:19:25 -0400

    adds database connection logic and table creation

---
+++ db.php
@@ -0,0 +1,29 @@
+<?php
+try {
+    // connects to the external database
+    $db = new PDO('sqlite:./db/skillswap.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // creates users table
+    $db->exec("CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    username TEXT UNIQUE
+    email TEXT UNIQUE
+    phone_number TEXT UNIQUE
+    password TEXT
+    )");
+
+    // creates skills table
+    $db->exec("CREATE TABLE IF NOT EXISTS skills (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER, 
+    skill TEXT, 
+    type TEXT CHECK(type IN('teach','learn')), 
+    FOREIGN KEY(user_id) REFERENCES users(id)
+    )");
+
+    echo "Database and tables created successfully!";
+} catch (PDOException $e) {
+    echo "Database Error: " . $e->getMessage();
+}
+?>
